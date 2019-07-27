e46dcbab99171aacbfa594ec2bb2a5d84103d035
unknown <sapphon@gmail.com>
2019-07-27 16:54:49 -0400

    Initial source commit; 100+ errors

---
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/command/CommandMPGetPlayerLookVector.java
@@ -0,0 +1,16 @@
+package org.sapphon.minecraft.modding.minecraftpython.command;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.Vec3d;
+
+public class CommandMPGetPlayerLookVector {
+	public double[] execute(){
+		Minecraft minecraft = Minecraft.getMinecraft();
+		Vec3d lookVector = minecraft.player.getLook(1.0f);
+		return new double[]{
+			lookVector.x,
+			lookVector.y,
+			lookVector.z
+		};
+	}
+}
