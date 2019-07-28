17f5842df5f75fbd7c78c715d8403726cda05801
unknown <sapphon@gmail.com>
2019-07-28 01:03:17 -0400

    Bring TechMageMod into new lifecycle

--- src/main/java/org/sapphon/minecraft/modding/techmage/TechMageMod.java
+++ src/main/java/org/sapphon/minecraft/modding/techmage/TechMageMod.java
@@ -1,6 +1,10 @@
 package org.sapphon.minecraft.modding.techmage;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.sapphon.minecraft.modding.base.ModConfigurationFlags;
 import org.sapphon.minecraft.modding.minecraftpython.ScriptLoaderConstants;
@@ -11,22 +15,35 @@ public class TechMageMod {
 
 	public static TechMageMod instance;
 
-	public void preInit(FMLPreInitializationEvent event) {
+	public TechMageMod(){
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doCommonInit);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientInit);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
+	public void doClientInit(FMLClientSetupEvent event){
 		if (isEnabled()) {
-			if(!ScriptLoaderConstants.resourcePathExists()){
+			if (!ScriptLoaderConstants.resourcePathExists()) {
 				ScriptLoaderConstants.setResourcePath(event);
 			}
-				for (MagicWand wand : ArcaneArmory.SINGLETON().getWands()) {
-					wand.setRegistryName(new ResourceLocation("techmage", wand.getUnlocalizedName()));
-					ForgeRegistries.ITEMS.register(wand);
-					String wandDisplayName = wand.getSpell().getDisplayName();
-					if (wandDisplayName != SpellMetadataConstants.NONE) {
-						/*LanguageRegistry.instance().addStringLocalization(
-								wand.getUnlocalizedName() + ".name", "en_US",
-								wandDisplayName);*/
-					}
-				}
+		}
+	}
+
+	public void doCommonInit(FMLCommonSetupEvent event) {
+		if (isEnabled()) {
+			registerWands();
+		}
+	}
+
+	private void registerWands() {
+		for (MagicWand wand : ArcaneArmory.SINGLETON().getWands()) {
+			ForgeRegistries.ITEMS.register(wand);
+			String wandDisplayName = wand.getSpell().getDisplayName();
+			if (wandDisplayName != SpellMetadataConstants.NONE) {
+				/*LanguageRegistry.instance().addStringLocalization(
+						wand.getUnlocalizedName() + ".name", "en_US",
+						wandDisplayName);*/
+			}
 		}
 	}
 
