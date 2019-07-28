17c8510b9d25c60c4aee06b0760acdea796a89a9
unknown <sapphon@gmail.com>
2019-07-27 23:13:27 -0400

    Fix BlockFinder

--- src/main/java/org/sapphon/minecraft/modding/base/BlockFinder.java
+++ src/main/java/org/sapphon/minecraft/modding/base/BlockFinder.java
@@ -1,12 +1,13 @@
 package org.sapphon.minecraft.modding.base;
 
 import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockFinder {
 	public static Block getBlockWithName(String name){
-		Block registryResult = Block.REGISTRY.getObject(new ResourceLocation(name.toLowerCase()));
+		Block registryResult = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(name.toLowerCase()));
 		return registryResult == null ? Blocks.DIRT : registryResult;
 	}
 
