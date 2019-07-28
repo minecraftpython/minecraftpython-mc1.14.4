a8c246663a4c96094bc5ef55206437a137fbd1f2
unknown <sapphon@gmail.com>
2019-07-27 23:47:49 -0400

    Update KeyHandler

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonKeyHandler.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonKeyHandler.java
@@ -1,15 +1,15 @@
 package org.sapphon.minecraft.modding.minecraftpython;
 
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.lwjgl.input.Keyboard;
 
 public class MinecraftPythonKeyHandler {
 	public static final int CAST_SPELL_KEY_INDEX = 0;
 	private static final String[] keyDescriptions = { "key.castspell.desc"};
-	private static final int[] defaultKeyValues = { Keyboard.KEY_P };
+	private static final int[] defaultKeyValues = {80};	//The 'P' key, I hope
 	private final KeyBinding[] keyBindings;
 	private IArcane device;
 	
