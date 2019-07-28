b1d8cb28c2901495cb9045310397be3e8d636a55
unknown <sapphon@gmail.com>
2019-07-27 23:56:21 -0400

    Delete unused CommonProxy

--- src/main/java/org/sapphon/minecraft/modding/base/CombinedClientProxy.java
+++ src/main/java/org/sapphon/minecraft/modding/base/CombinedClientProxy.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import org.sapphon.minecraft.modding.minecraftpython.command.ClientTickHandler;
 import org.sapphon.minecraft.modding.minecraftpython.command.ServerTickHandler;
 
-public class CombinedClientProxy extends CommonProxy {
+public class CombinedClientProxy  {
 	public CombinedClientProxy() {
 		FMLCommonHandler.instance().bus()
 				.register(new ClientTickHandler());
