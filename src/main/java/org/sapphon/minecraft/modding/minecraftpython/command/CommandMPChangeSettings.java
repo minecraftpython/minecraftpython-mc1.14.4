1fbd2225c1320a6b3ca891e01604c9ee41ee669b
unknown <sapphon@gmail.com>
2019-07-28 02:06:48 -0400

    ChangeSettings command now sets fields directly

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/command/CommandMPChangeSettings.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/command/CommandMPChangeSettings.java
@@ -1,7 +1,6 @@
 package org.sapphon.minecraft.modding.minecraftpython.command;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.GameSettings.Options;
 
 public class CommandMPChangeSettings extends CommandMinecraftPythonClient {
 
@@ -17,11 +16,10 @@ public class CommandMPChangeSettings extends CommandMinecraftPythonClient {
 
 	@Override
 	public void doWork() {
-
-			Minecraft.getInstance().gameSettings.setOptionFloatValue(Options.FOV, 0);
-			Minecraft.getInstance().gameSettings.setOptionValue(Options.RENDER_DISTANCE, Math.min(Minecraft.getInstance().gameSettings.renderDistanceChunks, 4));
-			Minecraft.getInstance().gameSettings.setOptionValue(Options.FRAMERATE_LIMIT, 24);
-			Minecraft.getInstance().gameSettings.setOptionValue(Options.GRAPHICS, 0);
+			Minecraft.getInstance().gameSettings.fov = 0;
+			Minecraft.getInstance().gameSettings.renderDistanceChunks = Math.min(Minecraft.getInstance().gameSettings.renderDistanceChunks, 4);
+			Minecraft.getInstance().gameSettings.framerateLimit = 24;
+			Minecraft.getInstance().gameSettings.fancyGraphics = false;
 			Minecraft.getInstance().gameSettings.saveOptions();
 	}
 
