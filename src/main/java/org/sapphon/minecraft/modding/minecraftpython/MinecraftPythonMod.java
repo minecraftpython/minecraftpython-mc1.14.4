280a6245c7faaf8c3a4c1732103533293dd594c1
unknown <sapphon@gmail.com>
2019-07-27 21:56:38 -0400

    Bring MP mod file into line with new Forge lifecycle (must redesign Proxy classes though!)

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonMod.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonMod.java
@@ -1,68 +1,58 @@
 package org.sapphon.minecraft.modding.minecraftpython;
 
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.sapphon.minecraft.modding.base.ClientProxy;
-import org.sapphon.minecraft.modding.base.CommonProxy;
-import org.sapphon.minecraft.modding.base.DedicatedServerProxy;
 import org.sapphon.minecraft.modding.base.ModConfigurationFlags;
 import org.sapphon.minecraft.modding.minecraftpython.command.PacketHandlerMinecraftPythonServerCommand;
 import org.sapphon.minecraft.modding.minecraftpython.command.PacketMinecraftPythonServerCommand;
 import org.sapphon.minecraft.modding.minecraftpython.spells.ThreadFactory;
 
-@Mod(modid = MinecraftPythonMod.MODID, version = MinecraftPythonMod.VERSION, name = MinecraftPythonMod.MODID)
+@Mod("minecraftpython")
 public class MinecraftPythonMod {
 	public static final String MODID = "minecraftpython";
 	public static final String VERSION = "1.10.2-0.5.0";
 	public static final int SCRIPT_RUN_COOLDOWN = 1500;
 	public static final Logger logger = LogManager.getLogger(MinecraftPythonMod.MODID);
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
+
+	public MinecraftPythonMod(){
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doSharedInit);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientInit);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	@Mod.Instance(value = MinecraftPythonMod.MODID)
 	public static MinecraftPythonMod instance;
 
-	@SidedProxy(clientSide = "org.sapphon.minecraft.modding.base.CombinedClientProxy", serverSide = "org.sapphon.minecraft.modding.base.DedicatedServerProxy")
-	public static CommonProxy proxy;
-	public static SimpleNetworkWrapper serverCommandPacketChannel;
-	public static SimpleNetworkWrapper clientCommandPacketChannel;
+	public static SimpleChannel serverCommandPacketChannel;
+	public static SimpleChannel clientCommandPacketChannel;
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
+	private void doSharedInit(final FMLCommonSetupEvent event)
+	{
 		if (isEnabled()) {
 			if(!ScriptLoaderConstants.resourcePathExists()){
 				ScriptLoaderConstants.setResourcePath(event);
 			}
-				serverCommandPacketChannel = NetworkRegistry.INSTANCE
-						.newSimpleChannel("MPServerCommand");
-				serverCommandPacketChannel.registerMessage(
-						PacketHandlerMinecraftPythonServerCommand.class,
-						PacketMinecraftPythonServerCommand.class, 0, Side.SERVER);
-				clientCommandPacketChannel = NetworkRegistry.INSTANCE
-						.newSimpleChannel("MPClientCommand");
-				clientCommandPacketChannel.registerMessage(
-						PacketHandlerMinecraftPythonClientCommand.class,
-						PacketMinecraftPythonClientCommand.class, 0, Side.CLIENT);
+			serverCommandPacketChannel = NetworkRegistry.INSTANCE
+					.newSimpleChannel("MPServerCommand");
+			serverCommandPacketChannel.registerMessage(
+					PacketHandlerMinecraftPythonServerCommand.class,
+					PacketMinecraftPythonServerCommand.class, 0, Side.SERVER);
+			clientCommandPacketChannel = NetworkRegistry.INSTANCE
+					.newSimpleChannel("MPClientCommand");
+			clientCommandPacketChannel.registerMessage(
+					PacketHandlerMinecraftPythonClientCommand.class,
+					PacketMinecraftPythonClientCommand.class, 0, Side.CLIENT);
 		}
 	}
 
-	private boolean isEnabled() {
-		return ModConfigurationFlags.MINECRAFT_PYTHON_PROGRAMMING();
-	}
-
-	@Mod.EventHandler
-	public void load(FMLInitializationEvent event) {
-		if (isEnabled() && !(proxy instanceof DedicatedServerProxy)) {
+	private void doClientInit(final FMLClientSetupEvent event) {
+		if (isEnabled()) {
 			FMLCommonHandler
 					.instance()
 					.bus()
@@ -70,15 +60,15 @@ public class MinecraftPythonMod {
 							new MinecraftPythonKeyHandler(
 									MinecraftPythonScriptLoader.SINGLETON()
 											.getMagicVessel()));
+
+			if (ModConfigurationFlags.MPPM_WEB()) {
+				ThreadFactory.makeJavaGameLoopThread().start();
+			}
 		}
-		proxy.registerRenderers();
 	}
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-		if (!(proxy instanceof ClientProxy) && ModConfigurationFlags.MPPM_WEB()) {
-			ThreadFactory.makeJavaGameLoopThread().start();
-		}
+	private boolean isEnabled() {
+		return ModConfigurationFlags.MINECRAFT_PYTHON_PROGRAMMING();
 	}
 	
 }
