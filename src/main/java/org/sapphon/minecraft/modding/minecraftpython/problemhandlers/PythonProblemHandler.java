f1f346377a4b17d8629679ae593df47fb287856e
Sapphon <sapphon@gmail.com>
2020-02-25 16:19:16 -0500

    Remove branching structure of ProblemHandler

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/problemhandlers/PythonProblemHandler.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/problemhandlers/PythonProblemHandler.java
@@ -1,7 +1,6 @@
 package org.sapphon.minecraft.modding.minecraftpython.problemhandlers;
 
 import net.minecraft.client.Minecraft;
-import org.sapphon.minecraft.modding.base.MsgBox;
 import org.sapphon.minecraft.modding.gui.GuiPythonErrorMessage;
 
 public class PythonProblemHandler extends AbstractProblemHandler {
@@ -9,15 +8,10 @@ public class PythonProblemHandler extends AbstractProblemHandler {
     public static void printErrorMessageToDialogBox(Exception e) {
         String fullErrorMessage = getStackTraceFromException(e);
         String pythonErrorMessageFull = fullErrorMessage.split("at org.")[0];
-        String pythonErrorMessageErrorOnly = pythonErrorMessageFull.split("\r\n")[2];
-        if (ITS_FOR_THE_KIDS) {
-            MsgBox.error(pythonErrorMessageErrorOnly, "Python Interpreter Failure - See Below For Error Message");
-        } else {
-            Minecraft minecraft = Minecraft.getInstance();
-            if (minecraft != null) {
-                System.out.println(fullErrorMessage);
-                minecraft.displayGuiScreen(new GuiPythonErrorMessage(pythonErrorMessageFull));
-            }
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft != null) {
+            System.out.println(fullErrorMessage);
+            minecraft.displayGuiScreen(new GuiPythonErrorMessage(pythonErrorMessageFull));
         }
     }
 }
